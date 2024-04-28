89aa332c4250781b698a1bfc6d8891e9ca925811
ManishaMartina <manishamartinaeee@gmail.com>
2024-04-28 15:02:33 +0530

    Add files via upload

---
+++ bluetooth with 8051.c
@@ -0,0 +1,69 @@
+#include<reg51.h>
+sbit IN1=P2^0;
+sbit IN2=P2^1;
+sbit IN3=P2^2;
+sbit IN4=P2^3;
+void ser_int();
+unsigned char rx();
+void tx(unsigned char a);
+void main()
+{
+unsigned char get;
+ser_int();
+while(1)
+{
+	get=rx();
+	tx(get);
+	if(get=='f')
+	{
+IN1=1;
+IN3=1;
+IN2=0;
+IN4=0;
+}
+else if (get=='R')
+{
+IN2=1;
+IN4=1;
+IN1=0;
+IN3=0;
+}
+else if (get=='W')
+{
+IN1=1;
+IN2=0;
+IN3=0;
+IN4=0;
+}
+else if (get=='L')
+{
+IN2=1;
+IN4=0;
+IN1=0;
+IN3=0;
+}
+else if (get=='S')
+{	
+IN1=IN2=IN3=IN4=0;
+}
+}
+void ser_int()
+{
+SCON=0X50;
+TMOD=0X20;
+TH1=TL1=0XFD;
+TR1;
+}
+unsigned char rx()
+{
+while(RI==0);
+RI=0;
+return SBUF;
+}
+void tx(unsigned char a)
+{
+SBUF=a;
+while(T1==0);
+T1=0;
+}
+}
